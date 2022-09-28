3534b2dd3f6edc47988f95ebf235339bf1abc695
riasvdv <rias@spatie.be>
2022-09-28 15:34:15 +0000

    wip

--- src/PostmarkWebhookController.php
+++ src/PostmarkWebhookController.php
@@ -3,16 +3,36 @@
 namespace Spatie\MailcoachPostmarkFeedback;
 
 use Illuminate\Http\Request;
+use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 use Spatie\WebhookClient\WebhookProcessor;
 
 class PostmarkWebhookController
 {
+    use UsesMailcoachModels;
+
     public function __invoke(Request $request)
     {
+        $this->registerMailerConfig($request->route('mailer'));
+
         $webhookConfig = PostmarkWebhookConfig::get();
 
         (new WebhookProcessor($request, $webhookConfig))->process();
 
         return response()->json(['message' => 'ok']);
+    }
+
+    public function registerMailerConfig(?string $mailer): void
+    {
+        if (! $mailer) {
+            return;
+        }
+
+        $mailer = cache()->remember(
+            "mailcoach-mailer-{$mailer}",
+            now()->addMinute(),
+            fn () => self::getMailerClass()::findByConfigKeyName($mailer),
+        );
+
+        $mailer?->registerConfigValues();
     }
 }
