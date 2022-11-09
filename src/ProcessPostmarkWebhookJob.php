f145f80b7ca2f4787ed487bad4dc573735b8005b
riasvdv <rias@spatie.be>
2022-11-09 10:58:34 +0000

    Update split workflow

--- src/ProcessPostmarkWebhookJob.php
+++ src/ProcessPostmarkWebhookJob.php
@@ -5,8 +5,8 @@ namespace Spatie\MailcoachPostmarkFeedback;
 use Illuminate\Support\Arr;
 use Spatie\Mailcoach\Domain\Campaign\Events\WebhookCallProcessedEvent;
 use Spatie\Mailcoach\Domain\Shared\Models\Send;
-use Spatie\Mailcoach\Domain\Shared\Support\Config;
 use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
+use Spatie\Mailcoach\Mailcoach;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
 use Spatie\WebhookClient\Models\WebhookCall;
 
@@ -20,7 +20,7 @@ class ProcessPostmarkWebhookJob extends ProcessWebhookJob
 
         $this->queue = config('mailcoach.campaigns.perform_on_queue.process_feedback_job');
 
-        $this->connection = $this->connection ?? Config::getQueueConnection();
+        $this->connection = $this->connection ?? Mailcoach::getQueueConnection();
     }
 
     public function handle()
